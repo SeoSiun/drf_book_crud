78a5ab86146077db3d780c7569a9919de928f647
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-09-06 14:16:12 +0900

    book model,serializer 생성 및 swagger 세팅
    1. api/models.py에 Book 모델 생성
       - field: title, author, introduction, price
    
    2. api/serializer.py 및 BookSerializer 생성
    
    3. api/urls.py 생성 및 router 적용
    
    4. api/view.py에 BookViewSet 생성
    
    5. book_crud/urls.py에 swagger 경로 추가

--- book_crud/api/views.py
+++ book_crud/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Book
+from .serializer import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
