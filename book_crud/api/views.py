357a450d58f1e8016968422e9f9cb708d1720997
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-09-06 18:23:02 +0900

    BookViewSet에서 swagger 설명 및 책 검색 기능 추가
    - get_queryset()을 오버라이드해서 title, author로 책을 검색하는 기능 추가.
    - @swagger_auto_schema를 이용해 각 api에 대한 설명 추가.

--- book_crud/api/views.py
+++ book_crud/api/views.py
@@ -1,8 +1,62 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from rest_framework.response import Response
 
 from .models import Book
-from .serializer import BookSerializer
+from .serializers import BookSerializer
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        search_title = self.request.query_params.get('title', )
+        search_author = self.request.query_params.get('author', )
+        if search_title:
+            qs = qs.filter(title__contains=search_title)
+        if search_author:
+            qs = qs.filter(author__contains=search_author)
+        return qs
+
+    @swagger_auto_schema(
+        operation_summary='책 목록',
+        manual_parameters=[
+            openapi.Parameter('title', openapi.IN_QUERY, type=openapi.TYPE_STRING),
+            openapi.Parameter('author', openapi.IN_QUERY, type=openapi.TYPE_STRING),
+        ],
+    )
+    def list(self, request, *args, **kwargs):
+        """query parameter가 없다면 전체 목록, 있다면 query로 받은 title, author를 포함하는 책 목록을 가져옴."""
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @swagger_auto_schema(operation_summary='책 생성')
+    def create(self, request, *args, **kwargs):
+        """title, author, introduction, price 정보를 갖는 책 생성."""
+        return super(BookViewSet, self).create(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_summary='책 detail 정보')
+    def retrieve(self, request, *args, **kwargs):
+        """id에 해당하는 책의 정보를 가져옴."""
+        return super(BookViewSet, self).retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_summary='책 정보 전체 수정')
+    def update(self, request, *args, **kwargs):
+        """id에 해당하는 책의 title, author, introduction, price를 request로 받은 값으로 수정"""
+        return super(BookViewSet, self).update(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_summary='책 정보 수정')
+    def partial_update(self, request, *args, **kwargs):
+        """id에 해당하는 책의 title, author, introduction, price 중 request body에서 받은 값을 수정"""
+        return super(BookViewSet, self).partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_summary='책 삭제')
+    def destroy(self, request, *args, **kwargs):
+        """id에 해당하는 책을 삭제"""
+        return super(BookViewSet, self).destroy(request, *args, **kwargs)
