103e3194229f5173f6f456df562b1a487b8b59ef
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-09-07 17:10:59 +0900

    책 목록 ([Get] /books/) swagger api 설명 수정
    - query parameter에 책 목록 정렬을 위한 ordering 추가
    
    - query parameter 각각에 대한 설명 추가
        - title, author는 검색 기능을 위한 것이고, ordering은 정렬 기능을 위함.
    
    - 책 목록 api 설명에 title, author, price 기준으로 정렬할 수 있다는 내용 추가.

--- book_crud/api/views.py
+++ book_crud/api/views.py
@@ -38,12 +38,18 @@ class BookViewSet(viewsets.ModelViewSet):
     @swagger_auto_schema(
         operation_summary='책 목록',
         manual_parameters=[
-            openapi.Parameter('title', openapi.IN_QUERY, type=openapi.TYPE_STRING),
-            openapi.Parameter('author', openapi.IN_QUERY, type=openapi.TYPE_STRING),
+            openapi.Parameter('title', openapi.IN_QUERY, type=openapi.TYPE_STRING,
+                              description='값이 주어진다면 title에 이 값을 포함하는 책 목록을 가져옵니다.(검색어)'),
+            openapi.Parameter('author', openapi.IN_QUERY, type=openapi.TYPE_STRING,
+                              description='값이 주어진다면 author에 이 값을 포함하는 책 목록을 가져옵니다.(검색어)'),
+            openapi.Parameter('ordering', openapi.IN_QUERY, type=openapi.TYPE_STRING,
+                              description='이 값(field 이름)을 기준으로 오름차순으로 정렬 (field 명 앞에 "-"를 붙이면 내림차순) '
+                                          + '/ 정렬 기준이 될 수 있는 field는 title, author, price '
+                                          + '/ 올바르지 않은 값이 주어지거나, ordering을 넘기지 않으면 id를 기준으로 오름차순으로 정렬.'),
         ],
     )
     def list(self, request, *args, **kwargs):
-        """query parameter가 없다면 전체 목록, 있다면 query로 받은 title, author를 포함하는 책 목록을 가져옴."""
+        """query parameter에 따라 title, author로 검색하거나, title, author, price를 기준으로 정렬된 책 목록을 가져옴."""
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
